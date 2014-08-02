f43b8fab8e3b1977f9a8ecea23e0b247ad6fe069
tyrannosaurus <trex@require.pm>
2014-08-02 15:24:12 +0400

    AutoPEP8

--- kyoto/conf/__init__.py
+++ kyoto/conf/__init__.py
@@ -1,24 +1,25 @@
 import os
 import importlib
-
 from kyoto.conf import default_settings
+SETTINGS_MODULE = os.environ.get("KYOTO_SETTINGS_MODULE", None)
 
 
 class Settings(object):
 
-  def __init__(self):
-    settings_module = os.environ.get("KYOTO_SETTINGS_MODULE", "kyoto.conf.default_settings")
-    for key in dir(default_settings):
-      self.__dict__[key] = getattr(default_settings, key)
-    try:
-      settings_module = importlib.import_module(settings_module)
-    except ImportError:
-      raise ImportError("Could not import settings module: '{0}'".format(settings))
-    else:
-      for key in dir(settings_module):
-        if key.isupper():
-          value = getattr(settings_module, key)
-          if not callable(value):
-            self.__dict__[key] = value
+    def __init__(self):
+        for key in dir(default_settings):
+            self.__dict__[key] = getattr(default_settings, key)
+        if SETTINGS_MODULE:
+            try:
+                settings_module = importlib.import_module(SETTINGS_MODULE)
+            except ImportError:
+                message = "Could not import settings module: '{0}'"
+                raise ImportError(message.format(settings))
+            else:
+                for key in dir(settings_module):
+                    if key.isupper():
+                        value = getattr(settings_module, key)
+                        if not callable(value):
+                            self.__dict__[key] = value
 
 settings = Settings()
--- kyoto/conf/default_settings.py
+++ kyoto/conf/default_settings.py
@@ -18,8 +18,8 @@ INSTALLED_MODULES = ()
 Network settings
 """
 
-MAX_BERP_SIZE = 33554432 # 32 megabytes
-READ_CHUNK_SIZE = 65536 # 64 kilobytes
+MAX_BERP_SIZE = 33554432  # 32 megabytes
+READ_CHUNK_SIZE = 65536  # 64 kilobytes
 COMPRESS_RESPONSE = False
 DISABLE_NAGLE = True
 
@@ -28,27 +28,27 @@ Logging settings
 """
 
 LOGGING = {
-  "version": 1,
-  "disable_existing_loggers": False,
+    "version": 1,
+    "disable_existing_loggers": False,
 
-  "formatters": {
-    "standard": {
-      "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        },
     },
-  },
-  "handlers": {
-    "default": {
-      "level":"INFO",
-      "class":"logging.StreamHandler",
+    "handlers": {
+        "default": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
     },
-  },
-  "loggers": {
-    "": {
-      "handlers": ["default"],
-      "level": "INFO",
-      "propagate": True
+    "loggers": {
+        "": {
+            "handlers": ["default"],
+            "level": "INFO",
+            "propagate": True
+        }
     }
-  }
 }
 
 
@@ -58,11 +58,11 @@ Blocking I/O related settings
 
 # list of modules which must be monkey-patched by gevent
 GEVENT_PATCH_MODULES = (
-  "os",
-  "ssl",
-  "time",
-  "select",
-  "socket",
+    "os",
+    "ssl",
+    "time",
+    "select",
+    "socket",
 )
 
 BLOCKING_POOL_SIZE = getattr(os, "cpu_count", multiprocessing.cpu_count)()
